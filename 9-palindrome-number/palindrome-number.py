f7869ea0136527dc010f3546ed8403c85eae12e7
Shivang Maurya <shivangmaurya2106@gmail.com>
2026-08-15 00:22:43 +0530

    Time: 21 ms (13.91%) | Memory: 12.3 MB (90.22%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+
+    def isPalindrome(self, x):
+
+        s = 0
+        num = x
+
+        while num > 0:
+            r = num % 10
+            s = (s * 10) + r
+            num = num // 10
+
+        if x == s:
+            return True
+        else:
+            return False
+
+
