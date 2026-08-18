8991945bab216ecb7f45ca70fca6d9df9c8c3c49
Shivang Maurya <shivangmaurya2106@gmail.com>
2026-08-18 23:49:40 +0530

    Time: 138 ms (27.91%) | Memory: 13.1 MB (35.73%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        c = ""
+
+        for ch in s.lower():
+            if ch.isalnum():
+                c += ch
+
+        return c == c[::-1]
